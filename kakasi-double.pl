bde48d9449b956ee9de43dec33fe12127cd9622a
Hirotaka Niitsuma <niitsuma@mub.biglobe.ne.jp>
2014-06-15 18:57:26 +0900

    oder swap roumaji

--- kakasi-double.pl
+++ kakasi-double.pl
@@ -6,12 +6,14 @@ use Text::Kakasi;
 
 
 while(<>) {
-                my $res2 = Text::Kakasi::getopt_argv('kakasi', split /\s+/, '-ja -ga -ka -Ea -Ka -Ha -Ja -U -s') ;
-		my $outtxt2=Text::Kakasi::do_kakasi( $_ );
-		print($outtxt2);
 
 		my $res1 = Text::Kakasi::getopt_argv('kakasi', ,split /\s+/, "-kK -w");
 		my $outtxt1=Text::Kakasi::do_kakasi( $_ );
 		print($outtxt1);
+
+                my $res2 = Text::Kakasi::getopt_argv('kakasi', split /\s+/, '-ja -ga -ka -Ea -Ka -Ha -Ja -U -s') ;
+		my $outtxt2=Text::Kakasi::do_kakasi( $_ );
+		print($outtxt2);
+
 }
 exit;
